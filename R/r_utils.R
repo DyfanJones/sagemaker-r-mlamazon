4b9dfa87a2990253611acbd30833ead4b56a7bff
Dyfan Jones <dyfan.jones@theverygroup.com>
2022-02-09 14:45:14 +0000

    doc: add credit to rlist package

--- R/r_utils.R
+++ R/r_utils.R
@@ -1,5 +1,7 @@
 `%||%` <- function(x, y) if (is.null(x)) return(y) else return(x)
 
+# Correctly mimic python append method for list
+# Full credit to package rlist: https://github.com/renkun-ken/rlist/blob/2692e064fc7b6cc7fe7079b3762df37bc25b3dbd/R/list.insert.R#L26-L44
 list.append <- function (.data, ...) {
   if (is.list(.data)) c(.data, list(...)) else c(.data, ..., recursive = FALSE)
 }
